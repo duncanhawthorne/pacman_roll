98eabf97dc19d7912119de6051f5d0cf3c940e38
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2024-05-04 12:11:04 +0100

    Update

--- lib/flame_game/endless_runner.dart
+++ lib/flame_game/endless_runner.dart
@@ -71,37 +71,18 @@ class EndlessRunner extends Forge2DGame<EndlessWorld>
 
     // With the `TextPaint` we define what properties the text that we are going
     // to render will have, like font family, size and color in this instance.
-    final textRenderer = TextPaint(
-      style: const TextStyle(
-        fontSize: 30,
-        color: Colors.white,
-        fontFamily: 'Press Start 2P',
-      ),
-    );
 
-    const scoreText = "Lives: 3";
-
-    // The component that is responsible for rendering the text that contains
-    // the current score.
-    final scoreComponent = TextComponent(
-      text: scoreText,
-      position: Vector2.all(30),
-      textRenderer: textRenderer,
-    );
 
     // The scoreComponent is added to the viewport, which means that even if the
     // camera's viewfinder move around and looks at different positions in the
     // world, the score is always static to the viewport.
-    camera.viewport.add(scoreComponent);
+
     camera.viewfinder.angle = 0;
 
     // Here we add a listener to the notifier that is updated when the player
     // gets a new point, in the callback we update the text of the
     // `scoreComponent`.
-    world.scoreNotifier.addListener(() {
-      scoreComponent.text =
-          scoreText.replaceFirst('3', '${3 - world.scoreNotifier.value}');
-    });
+
   }
 
   @override
@@ -112,6 +93,38 @@ class EndlessRunner extends Forge2DGame<EndlessWorld>
     }
     if (dxLast != dx || dyLast != dy) {
       camera.viewport = FixedResolutionViewport(resolution: Vector2(dx, dy));
+
+
+
+
+
+      final textRenderer = TextPaint(
+        style: const TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+          fontFamily: 'Press Start 2P',
+        ),
+      );
+
+      const scoreText = "Lives: 3";
+
+      // The component that is responsible for rendering the text that contains
+      // the current score.
+      final scoreComponent = TextComponent(
+        text: scoreText,
+        position: Vector2.all(30),
+        textRenderer: textRenderer,
+      );
+
+      world.scoreNotifier.addListener(() {
+        scoreComponent.text =
+            scoreText.replaceFirst('3', '${3 - world.scoreNotifier.value}');
+      });
+
+      camera.viewport.add(scoreComponent);
+
+
+
       dxLast = dx;
       dyLast = dy;
     }
