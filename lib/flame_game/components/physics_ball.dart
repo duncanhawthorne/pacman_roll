5adccfa49b9582b1c497a8b8aa5884e02bb478b8
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2024-05-09 22:15:55 +0100

    Update

--- lib/flame_game/components/physics_ball.dart
+++ lib/flame_game/components/physics_ball.dart
@@ -1,5 +1,4 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
-import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import 'game_character.dart';
--- lib/flame_game/components/wall.dart
+++ lib/flame_game/components/wall.dart
@@ -1,5 +1,4 @@
 import 'package:flame_forge2d/flame_forge2d.dart';
-import 'package:flame/collisions.dart';
 
 class Wall extends BodyComponent { //with CollisionCallbacks
   final Vector2 _start;
