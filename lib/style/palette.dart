23acd65a63df1b2ee30dde5853132d34f8330192
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2024-09-15 14:33:20 +0100

    Update

--- lib/style/palette.dart
+++ lib/style/palette.dart
@@ -2,20 +2,7 @@ import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 
 /// A palette of colors to be used in the game.
-///
-/// The reason we're not going with something like Material Design's
-/// `Theme` is simply that this is simpler to work with and yet gives
-/// us everything we need for a game.
-///
-/// Games generally have more radical color palettes than apps. For example,
-/// every level of a game can have radically different colors.
-/// At the same time, games rarely support dark mode.
-///
-/// Colors here are implemented as getters so that hot reloading works.
-/// In practice, we could just as easily implement the colors
-/// as `static const`. But this way the palette is more malleable:
-/// we could allow players to customize colors, for example,
-/// or even get the colors from the network.
+
 class Palette {
   static const seed = PaletteEntry(blueMaze); //Color(0xFF000000) //0xFF0050bc
   static const text =
