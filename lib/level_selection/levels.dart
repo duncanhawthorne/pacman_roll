37d2eca4e864b37f1345478c66949e48cdff0140
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2024-09-23 22:49:44 +0100

    Update

--- lib/level_selection/levels.dart
+++ lib/level_selection/levels.dart
@@ -1,93 +1,19 @@
-const gameLevels = <GameLevel>[
-  (
-    number: 0,
-    maxAllowedDeaths: 5,
-    superPelletsEnabled: true,
-    multipleSpawningGhosts: false,
-    ghostSpawnTimerLength: -1,
-    homingGhosts: false,
-  ),
-  (
-    number: 1,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: true,
-    multipleSpawningGhosts: false,
-    ghostSpawnTimerLength: -1,
-    homingGhosts: false,
-  ),
-  (
-    number: 2,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: false,
-    ghostSpawnTimerLength: -1,
-    homingGhosts: false,
-  ),
-  (
-    number: 3,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 5,
-    homingGhosts: false,
-  ),
-  (
-    number: 4,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 3,
-    homingGhosts: false,
-  ),
-  (
-    number: 5,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 2,
-    homingGhosts: false,
-  ),
-  (
-    number: 6,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 1,
-    homingGhosts: false,
-  ),
-  (
-    number: 7,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 5,
-    homingGhosts: true,
-  ),
-  (
-    number: 8,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 3,
-    homingGhosts: true,
-  ),
-  (
-    number: 9,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 2,
-    homingGhosts: true,
-  ),
-  (
-    number: 10,
-    maxAllowedDeaths: 3,
-    superPelletsEnabled: false,
-    multipleSpawningGhosts: true,
-    ghostSpawnTimerLength: 1,
-    homingGhosts: true,
-  ),
-];
+final gameLevels = List.generate(10 + 1, (index) => _gameLevelAtIndex(index));
+
+final _ghostSpawnTimerLengthPattern = {0: 5, 1: 3, 2: 2, 3: 1};
+
+GameLevel _gameLevelAtIndex(index) {
+  GameLevel result = (
+    number: index,
+    maxAllowedDeaths: index <= 0 ? 5 : 3,
+    superPelletsEnabled: index <= 1 ? true : false,
+    multipleSpawningGhosts: index <= 2 ? false : true,
+    ghostSpawnTimerLength:
+        index <= 2 ? -1 : _ghostSpawnTimerLengthPattern[(index - 3) % 4]!,
+    homingGhosts: index <= 6 ? false : true,
+  );
+  return result;
+}
 
 typedef GameLevel = ({
   int number,
